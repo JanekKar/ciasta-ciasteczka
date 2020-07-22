72de05ac33a7e1a1727627533ac965e9ca141382
Janek Karaszwski <jakarwski19@gmail.com>
2020-07-22 17:47:19 +0200

    Working navbar - desktop and mobile

--- app.js
+++ app.js
@@ -51,10 +51,10 @@ const closeNav = () => {
 
 // toggle submenu in mobile view 
 const subMenu = () => {
-    if(is_touch_device()){
+    if (is_touch_device()) {
         const subMenuToggle = document.querySelector('.sub-menu-toggle');
         const subMenu = subMenuToggle.querySelector('.sub-menu-toggle .sub-menu-1');
-    
+
         subMenuToggle.addEventListener('click', () => {
             subMenu.classList.toggle('sub-menu-active');
         })
@@ -87,13 +87,13 @@ const currentSection = () => {
 
 }
 
-function set_sticky(){
+function set_sticky() {
 
     const nav = document.querySelector('nav');
 
-    if(!is_touch_device()){
+    if (!is_touch_device()) {
         nav.classList.add('sticky');
-    }else{
+    } else {
         nav.classList.remove('sticky');
     }
 }
@@ -101,19 +101,19 @@ function set_sticky(){
 // check is media query matches mobile or desktop dimensions
 function is_touch_device() {
 
-    if ((window.matchMedia("(max-width: 768px)")).matches){
+    if ((window.matchMedia("(max-width: 768px)")).matches) {
         return true
-    }else{
+    } else {
         var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
-        
+
         var mq = function (query) {
             return window.matchMedia(query).matches;
         }
-    
+
         if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
             return true;
         }
-    
+
         // include the 'heartz' as a way to have a non matching MQ to help terminate the join
         // https://git.io/vznFH
         var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
@@ -122,8 +122,55 @@ function is_touch_device() {
     }
 }
 
-console.log(is_touch_device());
 
 set_sticky();
 subMenu();
 navSlide();
+
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active-dot", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active-dot";
+  }
+
+
+  window.addEventListener("load",function() {
+    showSlides(slideIndex);
+    myTimer = setInterval(function(){plusSlides(1)}, 4000);
+})
+
+
+function currentSlide(n){
+    clearInterval(myTimer);
+    myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
+    showSlides(slideIndex = n);
+  }
+
+
+function plusSlides(n){
+    clearInterval(myTimer);
+    if (n < 0){
+      showSlides(slideIndex -= 1);
+    } else {
+     showSlides(slideIndex += 1); 
+    }
+    if (n === -1){
+      myTimer = setInterval(function(){plusSlides(n + 2)}, 4000);
+    } else {
+      myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
+    }
+  }
+
+var slideIndex = 0;
+// carousel();
