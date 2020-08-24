53b72ac7ba00c5e15321296b61edc731afd3b600
Jan Karaszewski <jakarwski19@gmail.com>
2020-08-24 17:37:04 +0200

    Initial db migration and mysql engine downgrade

--- src/ciasta_ciasteczka/models.py
+++ src/ciasta_ciasteczka/models.py
@@ -1,20 +1,23 @@
 from django.db import models
 
+
 class Tray(models.Model):
     name = models.CharField(max_length=50)
     diameter = models.IntegerField()
     width = models.IntegerField()
     length = models.IntegerField()
 
+
 class Size(models.Model):
     name = models.CharField(max_length=50)
-    weight = models.FloatField(null = True)
-    tray = models.ForeignKey(Tray, null = True,on_delete=models.CASCADE)
+    weight = models.FloatField(null=True)
+    tray = models.ForeignKey(Tray, null=True, on_delete=models.CASCADE)
     amount_of_people = models.IntegerField()
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-    description =models.TextField(null=True)
+    description = models.TextField(null=True)
     parent = models.ForeignKey("Category", on_delete=models.CASCADE, null=True)
 
 
@@ -29,11 +32,13 @@ class Product(models.Model):
     size = models.ManyToManyField(Size)
     category = models.ManyToManyField(Category)
 
+
 class ProductPhoto(models.Model):
-    product = models.OneToOneField(Product, verbose_name="Product", on_delete=models.CASCADE)
+    product = models.OneToOneField(
+        Product, verbose_name="Product", on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     altr_text = models.CharField(max_length=100)
-    photo = models.ImageField(height_field=None, width_field=None, max_length=None)
 
-
+    photo = models.ImageField(
+        height_field=None, width_field=None, max_length=None)
 # Create your models here.
