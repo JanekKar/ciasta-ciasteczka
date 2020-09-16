3d23ba620194b9073eaa0c5308be76ab88fd91d8
Janek Karaszwski <jakarwski19@gmail.com>
2020-09-16 10:36:56 +0200

    Docker production and development  touch-ups

--- src/cc_project/settings.py
+++ src/cc_project/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
-        'OPTIONS': {'charset': 'utf8'}
+        'OPTIONS': {'charset': 'utf8',}
     }
 }
 
